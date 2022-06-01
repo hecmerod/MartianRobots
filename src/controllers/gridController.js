91bd42422cf2cae258ff8ff429072a20904407ad
hecmerod <hecmerod@gmail.com>
2022-06-01 19:26:25 +0200

    npx standard --fix

--- src/controllers/gridController.js
+++ src/controllers/gridController.js
@@ -3,19 +3,19 @@ const cache = require('../middleware/cache')
 exports.createGrid = async (req, res) => {
   const { x, y } = req.params
 
-  if(x <= 0 || y <= 0) {  
+  if (x <= 0 || y <= 0) {
     cache.del('grid')
-    cache.del('robot')  
-    
+    cache.del('robot')
+
     return res.send(
       `<div>
         <h2>Error</h2> 
         <p> x or y can't be negative, grid not created </p>
       </div>`
-      )
+    )
   }
 
-  cache.set('grid', { "x": parseInt(x), "y": parseInt(y) })
+  cache.set('grid', { x: parseInt(x), y: parseInt(y) })
   cache.del('robot')
 
   return res.send(
--- src/tests/grid.test.js
+++ src/tests/grid.test.js
@@ -5,28 +5,28 @@ const cache = require('../middleware/cache')
 const api = supertest(app)
 
 test('/grid/create/x/y creates a grid', async () => {
-    const x = 5, y = 6
+  const x = 5; const y = 6
 
-    await api.get(`/grid/create/${x}/${y}`).expect(200)
+  await api.get(`/grid/create/${x}/${y}`).expect(200)
 
-    const grid = cache.get('grid')
+  const grid = cache.get('grid')
 
-    expect(grid).toStrictEqual({'x': x, 'y': y})
+  expect(grid).toStrictEqual({ x, y })
 })
 
 test('x or y cant be negative', async () => {
-    var x = -5, y = 6
-    await api.get(`/grid/create/${x}/${y}`).expect(200)
-    var grid = cache.get('grid')
-    expect(grid).toBe(undefined)
+  let x = -5; let y = 6
+  await api.get(`/grid/create/${x}/${y}`).expect(200)
+  let grid = cache.get('grid')
+  expect(grid).toBe(undefined)
 
-    x = 5, y = -6
-    await api.get(`/grid/create/${x}/${y}`).expect(200)
-    grid = cache.get('grid')
-    expect(grid).toBe(undefined)
+  x = 5, y = -6
+  await api.get(`/grid/create/${x}/${y}`).expect(200)
+  grid = cache.get('grid')
+  expect(grid).toBe(undefined)
 
-    x = -5, y = -6
-    await api.get(`/grid/create/${x}/${y}`).expect(200)
-    grid = cache.get('grid')
-    expect(grid).toBe(undefined)
+  x = -5, y = -6
+  await api.get(`/grid/create/${x}/${y}`).expect(200)
+  grid = cache.get('grid')
+  expect(grid).toBe(undefined)
 })
